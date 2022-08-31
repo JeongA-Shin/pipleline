6b19081426b0ed81bbe5f58f9aafe3206985d4f6
신정아 <jeonga@khu.ac.kr>
2022-08-31 17:50:57 +0900

    init
    init project

---
+++ src/main/java/group/pipeline/webApi/BaseApi.java
@@ -0,0 +1,23 @@
+package group.pipeline.webApi;
+
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/pipe", produces = MediaType.APPLICATION_JSON_VALUE)
+@RestController("BaseApi")
+@RequiredArgsConstructor
+public class BaseApi {
+    
+    @SneakyThrows
+    //@ApiOperation("hello world")
+    @GetMapping()
+    public String getList(){
+        return "Hello world";
+    }
+}
---
+++ src/test/java/group/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package group.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
